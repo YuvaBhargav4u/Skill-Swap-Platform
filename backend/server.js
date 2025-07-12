de1c234e49087ea93b98f76c763ff583a3ac38a1
Nallamala Yuva Bhargav <cs24b028@iittp.ac.in>
2025-07-12 19:43:09 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -23,4 +23,6 @@ mongoose.connect(process.env.MONGO_URI, {
 app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log('Server running on port ${PORT}')
+});
